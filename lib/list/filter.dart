7d667e66c6e8f697915e6640f53f098cd583556f
MohamedElmdary <engm5081@gmail.com>
2021-07-24 21:19:36 +0200

    refactor list lib

--- lib/list/filter.dart
+++ lib/list/filter.dart
@@ -1,6 +1,6 @@
 import 'package:fp/types/curryFn.dart';
 
-List<T> _filter<T>(List<T> l, bool Function(T) p) {
+List<T> filter<T>(List<T> l, bool Function(T) p) {
   final List<T> res = [];
   int i = -1;
   while (++i < l.length) {
@@ -11,13 +11,9 @@ List<T> _filter<T>(List<T> l, bool Function(T) p) {
   return res;
 }
 
-List<T> filter<T>(List<T> list, bool Function(T) predicate) {
-  return _filter(list, predicate);
-}
-
 CurryFn<bool Function(T), List<T>> filterC<T>(List<T> list) {
   List<T> next(bool Function(T) predicate) {
-    return _filter(list, predicate);
+    return filter(list, predicate);
   }
 
   return next;
@@ -25,7 +21,7 @@ CurryFn<bool Function(T), List<T>> filterC<T>(List<T> list) {
 
 CurryFn<List<T>, List<T>> filterCR<T>(bool Function(T) predicate) {
   List<T> next(List<T> list) {
-    return _filter(list, predicate);
+    return filter(list, predicate);
   }
 
   return next;
--- lib/list/map.dart
+++ lib/list/map.dart
@@ -1,6 +1,6 @@
 import 'package:fp/types/curryFn.dart';
 
-List<R> _map<T, R>(List<T> list, R Function(T) mapper) {
+List<R> map<T, R>(List<T> list, R Function(T) mapper) {
   final List<R> res = [];
   int i = -1;
   while (++i < list.length) {
@@ -9,13 +9,9 @@ List<R> _map<T, R>(List<T> list, R Function(T) mapper) {
   return res;
 }
 
-List<R> map<T, R>(List<T> list, R Function(T) mapper) {
-  return _map(list, mapper);
-}
-
 CurryFn<R Function(T), List<R>> mapC<T, R>(List<T> list) {
   List<R> next(R Function(T) mapper) {
-    return _map(list, mapper);
+    return map(list, mapper);
   }
 
   return next;
@@ -23,7 +19,7 @@ CurryFn<R Function(T), List<R>> mapC<T, R>(List<T> list) {
 
 CurryFn<List<T>, List<R>> mapCR<T, R>(R Function(T) mapper) {
   List<R> next(List<T> list) {
-    return _map(list, mapper);
+    return map(list, mapper);
   }
 
   return next;
