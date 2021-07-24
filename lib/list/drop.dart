9cad2999168519ff05efa2cc9cc9be20bf15d03d
MohamedElmdary <engm5081@gmail.com>
2021-07-25 01:22:23 +0200

    added curry to drop function in list lib

--- lib/list/drop.dart
+++ lib/list/drop.dart
@@ -1,6 +1,24 @@
+import 'package:fp/types/curryFn.dart';
+
 List<T> drop<T>(List<T> list, int length) {
   if (length >= list.length) {
     return [];
   }
   return list.sublist(length);
+}
+
+CurryFn<int, List<T>> dropC<T>(List<T> list) {
+  List<T> next(int length) {
+    return drop(list, length);
+  }
+
+  return next;
+}
+
+CurryFn<List<T>, List<T>> dropCR<T>(int length) {
+  List<T> next(List<T> list) {
+    return drop(list, length);
+  }
+
+  return next;
 }
