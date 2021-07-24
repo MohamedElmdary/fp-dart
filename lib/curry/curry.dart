9eb224378b05ee213224495c7c0386c163486eb1
MohamedElmdary <engm5081@gmail.com>
2021-07-24 18:29:10 +0200

    added curry3 function

--- lib/curry/curry.dart
+++ lib/curry/curry.dart
@@ -1,1 +1,2 @@
 export 'curry2.dart';
+export 'curry3.dart';
---
+++ lib/curry/curry3.dart
@@ -0,0 +1,13 @@
+import 'package:fp/types/types.dart';
+import 'package:fp/curry/curry2.dart';
+
+CurryFn<P1, CurryFn<P2, CurryFn<P3, R>>> curry3<P1, P2, P3, R>(R Function(P1, P2, P3) fn) {
+  CurryFn<P2, CurryFn<P3, R>> next(P1 p1) {
+    R next(P2 p2, P3 p3) {
+      return fn(p1, p2, p3);
+    }
+    return curry2(next);
+  }
+  return next;
+}
+
