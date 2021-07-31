9150e3822464769e82c08d60c0b72e9cddfb77d3
MohamedElmdary <engm5081@gmail.com>
2021-07-31 18:27:41 +0200

    added maxBy function to relation lib

---
+++ lib/relation/maxBy.dart
@@ -0,0 +1,26 @@
+import 'package:fp/curry/curry2.dart';
+import 'package:fp/list/list.dart';
+
+num maxBy(
+  num Function(num) transformer,
+  List<num> nums,
+) {
+  if (nums.length == 0) {
+    return double.nan;
+  }
+
+  return reduce<num, num>(
+    (num res, num current) {
+      if (transformer(res) < transformer(current)) {
+        return current;
+      }
+
+      return res;
+    },
+    nums.first,
+    nums,
+  );
+}
+
+final maxByC = curry2(maxBy);
+final maxByCR = curry2R(maxBy);
