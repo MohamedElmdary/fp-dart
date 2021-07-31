097310f9724ae1270b9c5d50aaf9b243141ba0e6
MohamedElmdary <engm5081@gmail.com>
2021-07-31 20:30:33 +0200

    updated curry3_test

--- test/curry/curry3_test.dart
+++ test/curry/curry3_test.dart
@@ -3,32 +3,21 @@ import 'package:fp/curry/curry3.dart';
 
 void main() {
   group('Curry3', () {
-    final subThenDiv = (int a, int b, int by) => (a - b) / by;
+    /// it doesn't matter what its doing at all
+    final f = (int a, int b, int by) => (a - b) / by * a + b * 3.5;
 
     test('in order', () {
-      final subThenDivC = curry3(subThenDiv);
-      final subThenDivCWitha1 = subThenDivC(1);
+      final fC = curry3(f);
 
-      expect(subThenDivC(1)(1)(1), subThenDiv(1, 1, 1));
-      expect(subThenDivC(2)(7)(6), subThenDiv(2, 7, 6));
-      expect(subThenDivCWitha1(1)(1), subThenDiv(1, 1, 1));
-
-      // check for the value change
-      expect(subThenDivCWitha1(1)(1), subThenDiv(1, 1, 1));
+      expect(fC(1)(1)(1), f(1, 1, 1));
+      expect(fC(3)(4)(7), f(3, 4, 7));
     });
 
     test('reversed', () {
-      final subThenDivCR = curry3R(subThenDiv);
-      final by5 = subThenDivCR(5);
+      final fCR = curry3R(f);
 
-      final x = subThenDiv(2, 1, 5);
-
-      expect(subThenDivCR(1)(1)(1), subThenDiv(1, 1, 1));
-      expect(subThenDivCR(5)(1)(2), x);
-      expect(by5(1)(2), x);
-
-      // check for the value change
-      expect(by5(1)(2), x);
+      expect(fCR(1)(1)(1), f(1, 1, 1));
+      expect(fCR(7)(4)(3), f(3, 4, 7));
     });
   });
 }
