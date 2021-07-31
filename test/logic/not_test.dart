e44220711982736d307cdd9c4229fa3ec55af82d
MohamedElmdary <engm5081@gmail.com>
2021-08-01 00:31:25 +0200

    added not_test

--- test/logic/not_test.dart
+++ test/logic/not_test.dart
@@ -2,10 +2,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fp/logic/not.dart';
 
 void main() {
-  test('not', () {
-    final isFive = (int x) => x == 5;
+  group('Not', () {
+    final isFive = (v) => v == 5;
 
-    expect(not(isFive)(5), false);
-    expect(not(isFive)(0), true);
+    test('regular', () {
+      expect(not(isFive, 5), false);
+      expect(not(isFive, 0), true);
+    });
+
+    test('curry in order', () {
+      expect(notC(isFive)(5), false);
+      expect(notC(isFive)(0), true);
+    });
+
+    test('curry reversed', () {
+      expect(notCR(5)(isFive), false);
+      expect(notCR(0)(isFive), true);
+    });
   });
 }
