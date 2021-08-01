2f890ae953a967dff3240de86f0354c7309664ac
MohamedElmdary <engm5081@gmail.com>
2021-08-01 17:23:33 +0200

    updated: inc_test

--- test/math/inc_test.dart
+++ test/math/inc_test.dart
@@ -2,9 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fp/math/inc.dart';
 
 void main() {
-  test('Should increment number by 1.', () {
-    // regular
-    expect(inc(1), 2);
-    expect(inc(1.99), 2.99);
+  group('Inc', () {
+    test('regular', () {
+      expect(inc(1), 2);
+      expect(inc(7), 8);
+      expect(inc(-1), 0);
+    });
   });
 }
