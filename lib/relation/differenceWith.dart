12143b20e3e79b16c4eb5589cd53da1477009989
MohamedElmdary <engm5081@gmail.com>
2021-08-01 22:34:39 +0200

    update: added generic values to differenceWith

--- lib/relation/differenceWith.dart
+++ lib/relation/differenceWith.dart
@@ -1,7 +1,11 @@
-import 'package:fp/curry/curry3.dart';
+import 'package:fp/types/curryFn.dart';
 
-List differenceWith(bool Function(dynamic, dynamic) predicate, List x, List y) {
-  final List res = [];
+List<T> differenceWith<T, R>(
+  bool Function(T, R) predicate,
+  List<T> x,
+  List<R> y,
+) {
+  final List<T> res = [];
   int i = -1;
   while (++i < x.length) {
     int j = -1;
@@ -19,5 +23,30 @@ List differenceWith(bool Function(dynamic, dynamic) predicate, List x, List y) {
   return res;
 }
 
-final differenceWithC = curry3(differenceWith);
-final differenceWithCR = curry3R(differenceWith);
+Curry2Fn<List<T>, List<R>, List<T>> differenceWithC<T, R>(
+  bool Function(T, R) predicate,
+) {
+  CurryFn<List<R>, List<T>> next(List<T> x) {
+    List<T> next(List<R> y) {
+      return differenceWith(predicate, x, y);
+    }
+
+    return next;
+  }
+
+  return next;
+}
+
+Curry2Fn<List<T>, bool Function(T, R), List<T>> differenceWithCR<T, R>(
+  List<R> y,
+) {
+  CurryFn<bool Function(T, R), List<T>> next(List<T> x) {
+    List<T> next(bool Function(T, R) predicate) {
+      return differenceWith(predicate, x, y);
+    }
+
+    return next;
+  }
+
+  return next;
+}
