501612657d927c2e4adfe9a5ff1050816f9d4cdb
MohamedElmdary <engm5081@gmail.com>
2021-08-01 23:20:57 +0200

    refactor: compare function

--- lib/function/compare.dart
+++ lib/function/compare.dart
@@ -1,47 +1,29 @@
 import 'package:fp/object/prop.dart';
 
 abstract class Comparator {
-  bool boolC(bool x, bool y) {
-    return numC(x ? 1 : 0, y ? 1 : 0);
-  }
-
   bool numC(num x, num y);
   bool stringC(String x, String y);
   bool listC(dynamic x, dynamic y);
   bool mapC(Map x, Map y);
+  bool boolC(bool x, bool y) {
+    return numC(x ? 1 : 0, y ? 1 : 0);
+  }
 }
 
 bool Function(dynamic, dynamic) compare(Comparator c) {
   bool next(dynamic x, dynamic y) {
-    if (x.runtimeType != y.runtimeType) {
-      throw Exception('`gt` Expected the same type to be compared.');
-    }
-
-    if (x is num) {
-      return c.numC(x, y);
-    }
-
-    if (x is bool) {
-      return c.boolC(x, y);
-    }
-
-    if (x is String) {
-      return c.stringC(x, y);
-    }
-
-    if (x is List || x is Set) {
+    if (x is num && y is num) return c.numC(x, y);
+    if (x is bool && y is bool) return c.boolC(x, y);
+    if (x is String && y is String) return c.stringC(x, y);
+    if (x is Map && y is Map) return c.mapC(x, y);
+    if (x is ItoMap && y is ItoMap) return next(x.toMap(), y.toMap());
+    if ((x is List && y is List) || (x is Set && y is Set)) {
       return c.listC(x, y);
     }
 
-    if (x is Map) {
-      return c.mapC(x, y);
-    }
-
-    if (x is ItoMap) {
-      return next(x.toMap(), y.toMap());
-    }
-
-    throw Exception('unexpected type ${x.runtimeType}.');
+    throw Exception(
+      'Expected matched types but got [${x.runtimeType}, ${y.runtimeType}].',
+    );
   }
 
   return next;
