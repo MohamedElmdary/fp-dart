696b6b048949346f30519426076d1986c3481a70
MohamedElmdary <engm5081@gmail.com>
2021-08-01 17:27:01 +0200

    updated: mean_test

--- test/math/mean_test.dart
+++ test/math/mean_test.dart
@@ -2,10 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fp/math/mean.dart';
 
 void main() {
-  test('Should calc mean of the list.', () {
-    final List<int> list = [2, 7, 9];
-
-    expect(mean(list), 6);
-    expect(mean([]).toString(), "NaN");
+  group('Mean', () {
+    test('regular', () {
+      expect(mean([]).toString(), "NaN");
+      expect(mean([0, 1, 2, 3]), 1.5);
+      expect(mean([2, 7, 9]), 6);
+    });
   });
 }
