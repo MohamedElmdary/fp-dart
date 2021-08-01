1363f2f954b37e139f1a0494e4cf25305454bd6b
MohamedElmdary <engm5081@gmail.com>
2021-08-01 21:32:22 +0200

    fixed: re-write the entire deepEqual code

--- lib/function/deepEqual.dart
+++ lib/function/deepEqual.dart
@@ -3,71 +3,47 @@ import 'package:fp/object/keys.dart';
 import 'package:fp/object/prop.dart';
 
 bool deepEqual(dynamic x, dynamic y) {
-  if (x == y) {
-    return true;
-  }
+  if (x == y) return true;
 
-  if (x.runtimeType != y.runtimeType ||
-      !(x is Map) ||
-      !(x is List) ||
-      !(x is Set) ||
-      !(x is ItoMap)) {
-    return false;
-  }
+  if (x is Map) return _deepEqualMap(x, y);
+  if (x is Set || x is List) return _deepEqualList(x.toList(), y.toList());
+  if (x is ItoMap) return _deepEqualMap(x.toMap(), y.toMap());
 
-  // checking for x should be enough for x and y
-
-  if (x is List) {
-    if (x.length != y.length) {
-      return false;
-    }
-
-    int i = -1;
-    while (++i < x.length) {
-      int j = -1;
-      while (++j < x.length) {
-        if (deepEqual(x[i], y[j])) {
-          return true;
-        }
-      }
-    }
-
-    return false;
-  }
-
-  if (x is Map) {
-    final xKeys = keys(x);
-    final yKeys = keys(y);
-
-    if (xKeys.length != yKeys.length) {
-      return false;
-    }
-
-    int i = -1;
-    while (++i < xKeys.length) {
-      if (!y.containsKey(xKeys[i]) || !deepEqual(x[xKeys[i]], y[xKeys[i]])) {
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  if (x is Set) {
-    if (x.length != y.length) {
-      return false;
-    }
-
-    return deepEqual((x as dynamic).toList(), y.toList());
-  }
-
-  if (x is ItoMap && y is ItoMap) {
-    return deepEqual((x as dynamic).toMap(), y.toMap());
-  }
-
-  throw Exception(
-    'Unexpected types passed to deepEqual [`${x.runtimeType}`, `${y.runtimeType}`].',
-  );
+  return false;
 }
 
 final deepEqualC = curry2(deepEqual);
+
+bool _deepEqualList(List l1, List l2) {
+  if (l1.length != l2.length) return false;
+
+  int i = -1;
+  while (++i < l1.length) {
+    int j = -1;
+    while (++j < l2.length) {
+      if (deepEqual(l1[i], l2[j])) {
+        l2.removeAt(j);
+        break;
+      }
+    }
+  }
+
+  return l2.length == 0;
+}
+
+bool _deepEqualMap(Map m1, Map m2) {
+  if (m1.keys.length != m2.keys.length) return false;
+
+  final ks = keys(m1);
+
+  int i = -1;
+  while (++i < ks.length) {
+    final k = ks[i];
+
+    if (!m2.containsKey(k) || !deepEqual(m1[k], m2[k])) return false;
+
+    m2.remove(k);
+  }
+
+  return m2.keys.length == 0;
+}
