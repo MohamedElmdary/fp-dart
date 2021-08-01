74b448c1f8737ac484d15555bc9c77c670d9df80
MohamedElmdary <engm5081@gmail.com>
2021-08-01 17:13:36 +0200

    updated: add_test

--- test/math/add_test.dart
+++ test/math/add_test.dart
@@ -2,13 +2,19 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fp/math/add.dart';
 
 void main() {
-  test('Should add int or double.', () {
-    // regular
-    expect(add(1, 1), 2);
-    expect(add(1.2, 5), 6.2);
+  group('Add', () {
+    test('regular', () {
+      expect(add(1, 1), 2);
+      expect(add(1, -1), 0);
+      expect(add(1.5, 2), 3.5);
+      expect(add(1.5, -2), -0.5);
+    });
 
-    // curry
-    expect(addC(1)(1), 2);
-    expect(addC(7.534)(2.001), 9.535);
+    test('curry in order', () {
+      expect(addC(1)(1), 2);
+      expect(addC(1)(-1), 0);
+      expect(addC(1.5)(2), 3.5);
+      expect(addC(1.5)(-2), -0.5);
+    });
   });
 }
