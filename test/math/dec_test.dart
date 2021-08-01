73f6bca24fde2230c8e1f204049f6ed8b2fc6680
MohamedElmdary <engm5081@gmail.com>
2021-08-01 17:17:53 +0200

    updated: dec_test

--- test/math/dec_test.dart
+++ test/math/dec_test.dart
@@ -2,9 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fp/math/dec.dart';
 
 void main() {
-  test('Should decrement number by 1.', () {
-    // regular
-    expect(dec(1), 0);
-    expect(dec(1.99), 0.99);
+  group('Dec', () {
+    test('regular', () {
+      expect(dec(1), 0);
+      expect(dec(7), 6);
+      expect(dec(-1), -2);
+    });
   });
 }
