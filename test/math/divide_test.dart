48256602422c5fc209f3bdadc9b4d8b6935d1af1
MohamedElmdary <engm5081@gmail.com>
2021-08-01 17:22:22 +0200

    updated: divide_test

--- test/math/divide_test.dart
+++ test/math/divide_test.dart
@@ -2,15 +2,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fp/math/divide.dart';
 
 void main() {
-  test('Should decrement number by 1.', () {
-    // regular
-    expect(divide(1, 1), 1);
-    expect(divide(5, 2), 2.5);
-    expect(divide(1, 0), double.infinity);
+  group('Divide', () {
+    test('regular', () {
+      expect(divide(1, 0), double.infinity);
+      expect(divide(0, 0).toString(), "NaN");
+      expect(divide(0, 3.5), 0);
+      expect(divide(1, 1), 1);
+      expect(divide(1, 2), 0.5);
+      expect(divide(5, 2), 2.5);
+    });
 
-    // curry
-    expect(divideC(1)(1), 1);
-    expect(divideC(5)(2), 2.5);
-    expect(divideC(1)(0), double.infinity);
+    // skiped curry & curry reversed (using curry2)
   });
 }
