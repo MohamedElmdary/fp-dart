771dd1edbd7d72f84eb3382d69b405d57a533aa8
MohamedElmdary <engm5081@gmail.com>
2021-08-04 00:20:06 +0200

    added once_test

--- test/function/once_test.dart
+++ test/function/once_test.dart
@@ -2,12 +2,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fp/function/once.dart';
 
 void main() {
-  test('Should return same result everytime.', () {
-    final onceInc = once((int e) => e + 1);
+  group('Once', () {
+    final addOneOnce = once((num x) => x + 1);
 
-    expect(onceInc(10), 11);
-    expect(onceInc(20), 11);
-    expect(onceInc(30), 11);
-    expect(onceInc(100), 11);
+    test('regular', () {
+      expect(addOneOnce(10), 11);
+      expect(addOneOnce(13), 11);
+      expect(addOneOnce(15), 11);
+      expect(addOneOnce(105.5), 11);
+      expect(addOneOnce(27), 11);
+    });
   });
 }
