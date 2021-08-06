d1626315e48d50c12f7baeee4d4a5f04f6b737e7
MohamedElmdary <engm5081@gmail.com>
2021-08-06 23:40:56 +0200

    fixed: updated key accessing using J instead of I

--- lib/list/mergeAll.dart
+++ lib/list/mergeAll.dart
@@ -1,14 +1,17 @@
+import 'package:fp/object/keys.dart' as fp;
+
 Map<Key, Value> mergeAll<Key, Value>(List<Map<Key, Value>> list) {
   final Map<Key, Value> res = {};
 
   int i = -1;
   while (++i < list.length) {
     final Map<Key, Value> item = list[i];
-    final keys = item.keys.toList();
+    final keys = fp.keys(item);
 
     int j = -1;
     while (++j < keys.length) {
-      res[keys[j]] = item[keys[i]] as Value;
+      final k = keys[j];
+      res[k] = item[k] as Value;
     }
   }
   return res;
