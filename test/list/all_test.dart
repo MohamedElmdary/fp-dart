37162bae51fa410771ee8fa03f29156bc04ca985
MohamedElmdary <engm5081@gmail.com>
2021-08-06 16:34:36 +0200

    added all_test

---
+++ test/list/all_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fp/list/all.dart';
+
+void main() {
+  group('Compare', () {
+    final eq3 = (int v) => v == 3;
+
+    test('regular', () {
+      expect(all(eq3, [3, 3, 3]), true);
+      expect(all(eq3, [3, 1, 3]), false);
+    });
+
+    test('curry in order', () {
+      expect(allC(eq3)([3, 3, 3]), true);
+      expect(allC(eq3)([3, 1, 3]), false);
+    });
+
+    test('curry reversed', () {
+      expect(allCR([3, 3, 3])(eq3), true);
+      expect(allCR([3, 1, 3])(eq3), false);
+    });
+  });
+}
