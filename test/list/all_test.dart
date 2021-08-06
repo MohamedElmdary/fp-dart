bc9de0da723e1e08fb6686a177059614423726ad
MohamedElmdary <engm5081@gmail.com>
2021-08-06 16:39:41 +0200

    fixed group name is all_test

--- test/list/all_test.dart
+++ test/list/all_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fp/list/all.dart';
 
 void main() {
-  group('Compare', () {
+  group('All', () {
     final eq3 = (int v) => v == 3;
 
     test('regular', () {
