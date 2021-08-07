8489b4b41bd03d7ad5a2009dc0113142a96c7eec
MohamedElmdary <engm5081@gmail.com>
2021-08-07 16:12:43 +0200

    updated: omitPaths use dissocPath instead of omit

--- lib/object/omitPaths.dart
+++ lib/object/omitPaths.dart
@@ -1,10 +1,10 @@
 import 'package:fp/curry/curry2.dart';
 import 'package:fp/function/function.dart';
 import 'package:fp/list/list.dart';
-import 'package:fp/object/omit.dart';
+import 'package:fp/object/dissocPath.dart';
 
 Map omitPaths(List<List<String>> paths, Map obj) {
-  return composeC(map(paths, omitC))(obj);
+  return composeC(map(paths, dissocPathC))(obj);
 }
 
 final omitPathsC = curry2(omitPaths);
